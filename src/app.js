881720325e3afe143d7f70be59fad5bb9d756080
Mandar1jn <marijnkneppers@gmail.com>
2021-10-27 20:48:32 +0200

    got a view loaded instead of hard coding it

--- src/app.js
+++ src/app.js
@@ -2,16 +2,14 @@ const express = require("express");
 
 let app = express();
 
+app.set('view engine', "ejs");
+
 app.all("/profile/:username", async (req, res, err) => 
 {
 	const request = require("request");
 	request(`https://api.skyblockisles.com/player/${req.params.username}`, (error, response, body) => 
 	{
-		let json = JSON.parse(body);
-		let buf = Buffer.from(json["skin"][0], "base64");
-		console.log(json["skin"]);
-		let linkJson = JSON.parse(buf.toString());
-		res.send("username: " + json["username"] + "<br>UUID: " + json["uuid"] + "<br>Link to skin: " + linkJson["textures"]["SKIN"]["url"]);
+		res.render("profile", {name: "hello"});
 	});
 });
 
