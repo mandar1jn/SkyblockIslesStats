4ac12cb71e5ad97a16194cfa5c3496f8732acecf
Mandar1jn <marijnkneppers@gmail.com>
2021-10-28 07:58:24 +0200

    added basic skill stats
    Signed-off-by: Mandar1jn <marijnkneppers@gmail.com>

--- src/app.js
+++ src/app.js
@@ -9,7 +9,15 @@ app.all("/profile/:username", async (req, res, err) =>
 	const request = require("request");
 	request(`https://api.skyblockisles.com/player/${req.params.username}`, (error, response, body) => 
 	{
-		res.render("profile", {name: "hello"});
+		let json = JSON.parse(body);
+		if(body === "{}")
+		{
+			res.send("This user has not played on Skyblock Isles");
+		}
+		else
+		{
+			res.render("profile", {name: json["username"], skills: json["skills"]});
+		}
 	});
 });
 
